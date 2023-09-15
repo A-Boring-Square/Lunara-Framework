6de3e87639442e00058d27ed5a0acc449d204c49
A-Boring-Square <123091707+A-Boring-Square@users.noreply.github.com>
2023-09-15 17:23:06 -0500

    "reformatted things"

---
+++ Lunara/__init__p.py
@@ -0,0 +1,8 @@
+# lunara_game_framework/__init__.py
+
+# Import statements and other necessary code for your library go here.
+
+
+# Display the license information when the library is imported.
+print("Lunara Game Framework is licensed under the GNU General Public License v3.0 (GPL-3.0)")
+print("For more details, please refer to the LICENSE file included with this library.")
