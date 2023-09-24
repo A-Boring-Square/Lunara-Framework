6b8e87b5bb2575ddd2cca1ce8350af56e977b149
A-Boring-Square <123091707+A-Boring-Square@users.noreply.github.com>
2023-09-24 09:55:48 -0500

    Delete Lunara directory

--- Lunara/__init__p.py
+++
@@ -1,7 +0,0 @@
-from Renderer import *
-from Sound import *
-
-
-# Display the license information when the library is imported.
-print("Lunara Game Framework is licensed under the GNU General Public License v3.0 (GPL-3.0)")
-print("For more details, please refer to the LICENSE file included with this library.")
